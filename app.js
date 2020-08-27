22367cb027d1fe146b7287c3ca7f960cb46c8ff0
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-08-27 08:36:15 -0700

    add build folder flow to app.js

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const users = require("./routes/api/users");
 const scores = require("./routes/api/scores");
 module.exports = router;
 
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
